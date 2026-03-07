f8ff4cb2c4f85d52ee5bd03fa1a68a01b8dffa0c
Leilabangoy22 <bangoybangoy42@gmail.com>
2026-03-07 23:41:39 +0800

    Progress Report for Frontend

---
+++ K-Group_AMS_DBTC/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K_Group_AMS_DBTC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ K-Group_AMS_DBTC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace K_Group_AMS_DBTC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
