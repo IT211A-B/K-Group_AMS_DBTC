e5e637e466c257eb5977eb6ef361a2d4eb552859
K-Joshua <saintangel9989@gmail.com>
2026-05-01 08:32:42 +0800

    Fix: Fixed mistake deleted code block

--- Backend/Backend/Backend/Helper/AddRole.cs
+++ Backend/Backend/Backend/Helper/AddRole.cs
@@ -4,26 +4,22 @@ namespace Backend.Backend.Helper
 {
     public static class AddRole
     {
-        /// <summary>
         /// Add role through email, using this method
-        /// </summary>
-        /// <param name="email"></param>
-        /// <returns></returns>
         public static (Role? role, int status_code) AddRoleAccordingToEmail(this string email)
         {
             Role? role;
             int status_code;
-            if(email.Contains("@sup.user"))
-            {
-                role = Role.SUP;
-                status_code = 200;
-            } else if (email.Contains("@dbtc-cebu"))
+            if (email.Contains("@dbtc-cebu"))
             {
                 role = Role.STU;
                 status_code = 200;
             } else if (email.Contains("@local"))
             {
                 role = Role.TEA;
+                status_code = 200;
+            } else if (email.Contains("@admin"))
+            {
+                role = Role.ADM;
                 status_code = 200;
             } else
             {
