8caf998faced26d0a8815a63c2c99e4671660377
K-Joshua <saintangel9989@gmail.com>
2026-05-02 10:49:08 +0800

    Feat: Added Enum Role conversion into string helper

---
+++ Backend/Backend/Backend/Helper/EnumRoletoStringRole.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Backend.Helper.Enum;
+
+namespace Backend.Backend.Helper
+{
+    public static class EnumRoletoStringRole
+    {
+        public static string ConvertEnumRoletoStringRole(this PosEnum.PosStatus role)
+        {
+            string result = "";
+            switch (role)
+            {
+                case PosEnum.PosStatus.STU:
+                    result = "Student";
+                    break;
+                case PosEnum.PosStatus.TEA:
+                    result = "Teacher";
+                    break;
+                case PosEnum.PosStatus.ADM:
+                    result = "Admin";
+                    break;
+            }
+            return result;
+        }
+    }
+}
