700a4bd322c3ed1c6c491de8c4ceda309c730e40
K-Joshua <saintangel9989@gmail.com>
2026-05-02 10:36:21 +0800

    Feat: Added Config Validations and Service, specifically for auth generation and claims

---
+++ Backend/Backend/Backend/Configuration/PermissionRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Backend.Configuration
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+        public string Permission { get; }
+
+        public PermissionRequirement(string permission)
+        {
+            Permission = permission;
+        }
+    }
+}
