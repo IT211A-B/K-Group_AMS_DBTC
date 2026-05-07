af93d2223d3e4f46b2ee42439552f96163382813
K-Joshua <saintangel9989@gmail.com>
2026-05-07 14:45:07 +0800

    Feat: Added Section Controller

---
+++ Backend/Backend/Backend/DTO/SectionDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Backend.DTOs
+{
+    public class GetSectionDTO
+    {
+        public int Section_Id { get; set; }
+        public string? Section_Code { get; set; }
+    }
+
+    public class AddSectionDTO
+    {
+        public required string Section_Code { get; set; }
+    }
+}
