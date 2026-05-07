3a913f0aa7227307de5eddec9d9357413cfefde4
K-Joshua <saintangel9989@gmail.com>
2026-05-07 08:56:44 +0800

    Chore: Removed Cassy and Leila's Folders

--- Frontend/Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Frontend.Models
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; } = "";
-        public string Password { get; set; } = "";
-        public string Role { get; set; } = "admin";
-    }
-}
