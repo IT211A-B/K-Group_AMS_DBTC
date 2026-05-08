fd2246f1be99653d5d2176e0699e66c88644f45b
K-Joshua <saintangel9989@gmail.com>
2026-05-08 11:10:08 +0800

    Fix: Fixed Uuid secrecy using document series for the user

--- Backend/Backend/Backend/DTO/ScheduleDTO.cs
+++ Backend/Backend/Backend/DTO/ScheduleDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Backend.Backend.Model;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Backend.DTOs
@@ -7,10 +8,10 @@ namespace Backend.Backend.DTOs
     {
         public int Schedule_Id { get; set; }
         public required int Course_ID { get; set; }
-        public required int Course_Year { get; set; }
-        public required int Department_ID { get; set; }
-        public required int Program_ID { get; set; }
+        public required int Section_ID { get; set; }
         public required DayOfWeek DayOfWeek { get; set; }
+        public required int Semester { get; set; }
+        public required string AcademicYear { get; set; }
         public required TimeOnly StartTime { get; set; }
         public required TimeOnly EndTime { get; set; }
     }
@@ -18,10 +19,10 @@ namespace Backend.Backend.DTOs
     public class AddScheduleDTO
     {
         public required int Course_ID { get; set; }
-        public required int Course_Year { get; set; }
-        public required int Department_ID { get; set; }
-        public required int Program_ID { get; set; }
+        public required int Section_ID { get; set; }
         public required DayOfWeek DayOfWeek { get; set; }
+        public required int Semester { get; set; }
+        public required string AcademicYear { get; set; }
         public required TimeOnly StartTime { get; set; }
         public required TimeOnly EndTime { get; set; }
     }
