7d1481ea4abfa31acc8aa879cd4f2bce33bff0be
K-Joshua <saintangel9989@gmail.com>
2026-05-16 03:39:05 +0800

    Feat: Added Helper to Convert day Of Week to string

---
+++ Backend/Backend/Backend/Helper/StringToDayOfWeek.cs
@@ -0,0 +1,32 @@
+﻿namespace Backend.Backend.Helper
+{
+    public static class StringToDayOfWeek
+    {
+        public static DayOfWeek ConvertFromStringToDayOfWeek(this string day)
+        {
+            DayOfWeek result = DayOfWeek.Sunday;
+            switch (day)
+            {
+                case "Monday":
+                    result = DayOfWeek.Monday;
+                    break;
+                case "Tuesday":
+                    result = DayOfWeek.Tuesday;
+                    break;
+                case "Wednesday":
+                    result = DayOfWeek.Wednesday;
+                    break;
+                case "Thursday":
+                    result = DayOfWeek.Thursday;
+                    break;
+                case "Friday":
+                    result = DayOfWeek.Friday;
+                    break;
+                case "Saturday":
+                    result = DayOfWeek.Saturday;
+                    break;
+            }
+            return result;
+        }
+    }
+}
